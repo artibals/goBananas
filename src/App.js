e48038a5a04fa7db4662ff0be29a1d7b422fd404
Orko <artibals@gmail.com>
2021-10-13 11:14:23 +0300

    Started my project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './components/Header';
+import Banner from './components/Banner';
+import Main from './components/Main';
+import Footer from './components/Footer';
+
+const App = () => {
+    return (
+        <>
+            <Header />
+            <Banner />
+            <Main />
+            <Footer />
+        </>
+    );
+};
+
+export default App;
---
+++ src/components/Banner/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <>
+            <h2>Banner</h2>
+        </>
+    );
+};
+
+export default Banner;
